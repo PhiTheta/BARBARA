1c7995c637123deb566a38b245a66e0e31c03230
IVANY4 <ivany4@me.com>
2012-11-21 23:33:51 +0200

    points transformation method for simpleSLAM

--- simpleSLAM/main.cpp
+++ simpleSLAM/main.cpp
@@ -9,15 +9,17 @@ struct TPose2D {
     float x, y, angle;
 };
 
+struct TPoint {
+    float x, y;
+};
 
 //maxRadius how far the generated points can be from the current position
 //numRadiuses how many circles should be between maxRadius and current position
 //numPositions how many positions to generate in each circle
 //maxAngleDeviation how much the orientation can differ
 //numOrientations how many orientations to generate for each position
-std::vector<TPose2D> generatePoses(TPose2D currentPose, float maxRadius, int numRadiuses, int numPositions, float maxAngleDeviation, int numOrientations) {
-    std::vector<TPose2D> poses;
-    poses.begin();
+vector<TPose2D> generatePoses(TPose2D currentPose, float maxRadius, int numRadiuses, int numPositions, float maxAngleDeviation, int numOrientations) {
+    vector<TPose2D> poses;
     for (int i = 1; i <= numRadiuses; i++) {
         float r = maxRadius/i;
         for (int j = 1; j <= numPositions; j++) {
@@ -35,6 +37,25 @@ std::vector<TPose2D> generatePoses(TPose2D currentPose, float maxRadius, int num
     return poses;
 }
 
+vector<TPoint> transformPoints(TPose2D startPose, TPose2D endPose, vector<TPoint> points)
+{
+    vector<TPoint> transformedPoints;
+    float theta = startPose.angle - endPose.angle;
+    float tx = endPose.x - startPose.x;
+    float ty = endPose.y - startPose.y;
+    
+	for (vector<TPoint>::iterator iterator = points.begin(); iterator < points.end(); iterator++) {
+        TPoint point = *iterator;
+        float x = point.x;
+        float y = point.y;
+        TPoint newPoint;
+        newPoint.x = x*cos(theta)-y*sin(theta)+tx;
+        newPoint.y = x*sin(theta)+y*cos(theta)+ty;
+        transformedPoints.push_back(newPoint);
+	}
+    return transformedPoints;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -43,15 +64,40 @@ int main(int argc, char *argv[])
     currentPose.y = 2.7;
     currentPose.angle = 0;
     
-    std::vector<TPose2D> poses = generatePoses(currentPose, 0.5, 4, 8, 0.01, 5);
+    vector<TPose2D> poses = generatePoses(currentPose, 0.5, 4, 8, 0.01, 5);
     
-    std::cout << "Size: " << poses.size() << endl;
+    cout << "Size: " << poses.size() << endl;
     
-	for (std::vector<TPose2D>::iterator iterator = poses.begin(); iterator < poses.end(); iterator++) {
+	for (vector<TPose2D>::iterator iterator = poses.begin(); iterator < poses.end(); iterator++) {
         TPose2D pose = *iterator;
 		cout << "x: " << pose.x << "; y: " << pose.y << "; angle: " << pose.angle << endl;
 	}
 
+    vector<TPoint> points;
+    for (int i = 0; i < 10; i++) {
+        TPoint point;
+        point.x = i;
+        point.y = i;
+        points.push_back(point);
+    }
+    
+    cout << "Initial points:" << endl;
+	for (vector<TPoint>::iterator iterator = points.begin(); iterator < points.end(); iterator++) {
+        TPoint point = *iterator;
+		cout << "x: " << point.x << "; y: " << point.y << endl;
+	}
+    
+    cout << "Transformed points (270deg rotation, (1m; 0.3m) translation):" << endl;
+    cout << "Assuming that x axis extends to the east (relative to the robot), y - to the north, and positive angle is CCW" << endl;
+    TPose2D newPose;
+    newPose.x = 4;
+    newPose.y = 3;
+    newPose.angle = 3*M_PI_2;
+    vector<TPoint> newPoints = transformPoints(currentPose, newPose, points);
+	for (vector<TPoint>::iterator iterator = newPoints.begin(); iterator < newPoints.end(); iterator++) {
+        TPoint point = *iterator;
+		cout << "x: " << point.x << "; y: " << point.y << endl;
+	}
     
     
     return 0;
